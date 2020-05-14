47344d753dc25ee9226b1caeff7dce585bbcb8cd
fcakyon <fcakyon@gmail.com>
2020-05-14 20:05:22 +0300

    update basic usage for better device handling

--- craft_text_detector/__init__.py
+++ craft_text_detector/__init__.py
@@ -1,111 +1,162 @@
 from __future__ import absolute_import
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
-from craft_text_detector.imgproc import read_image
-
-from craft_text_detector.file_utils import export_detected_regions, export_extra_results
-
-from craft_text_detector.predict import (
-    load_craftnet_model,
-    load_refinenet_model,
-    get_prediction,
-)
-
-# load craft model
-craft_net = load_craftnet_model()
+import craft_text_detector.imgproc as imgproc
+import craft_text_detector.predict as predict
+import craft_text_detector.craft_utils as craft_utils
+import craft_text_detector.file_utils as file_utils
 
 
-# detect texts
-def detect_text(
-    image_path,
-    output_dir=None,
-    rectify=True,
-    export_extra=True,
-    text_threshold=0.7,
-    link_threshold=0.4,
-    low_text=0.4,
-    cuda=False,
-    long_size=1280,
-    show_time=False,
-    refiner=True,
-    crop_type="poly",
-):
-    """
-    Arguments:
-        image_path: path to the image to be processed
-        output_dir: path to the results to be exported
-        rectify: rectify detected polygon by affine transform
-        export_extra: export heatmap, detection points, box visualization
-        text_threshold: text confidence threshold
-        link_threshold: link confidence threshold
-        low_text: text low-bound score
-        cuda: Use cuda for inference
-        long_size: desired longest image size for inference
-        show_time: show processing time
-        refiner: enable link refiner
-        crop_type: crop regions by detected boxes or polys ("poly" or "box")
-    Output:
-        {"masks": lists of predicted masks 2d as bool array,
-         "boxes": list of coords of points of predicted boxes,
-         "boxes_as_ratios": list of coords of points of predicted boxes as ratios of image size,
-         "polys_as_ratios": list of coords of points of predicted polys as ratios of image size,
-         "heatmaps": visualization of the detected characters/links,
-         "text_crop_paths": list of paths of the exported text boxes/polys,
-         "times": elapsed times of the sub modules, in seconds}
-    """
-    # load image
-    image = read_image(image_path)
+__all__ = [
+    "read_image",
+    "load_craftnet_model",
+    "load_refinenet_model",
+    "get_prediction",
+    "export_detected_regions",
+    "export_extra_results",
+    "Craft",
+]
 
-    # load refiner if required
-    if refiner:
-        refine_net = load_refinenet_model()
-    else:
-        refine_net = None
+read_image = imgproc.read_image
+load_craftnet_model = craft_utils.load_craftnet_model
+load_refinenet_model = craft_utils.load_refinenet_model
+get_prediction = predict.get_prediction
+export_detected_regions = file_utils.export_detected_regions
+export_extra_results = file_utils.export_extra_results
 
-    # perform prediction
-    prediction_result = get_prediction(
-        image=image,
-        craft_net=craft_net,
-        refine_net=refine_net,
-        text_threshold=text_threshold,
-        link_threshold=link_threshold,
-        low_text=low_text,
-        cuda=cuda,
-        long_size=long_size,
-        show_time=show_time,
-    )
 
-    # arange regions
-    if crop_type == "box":
-        regions = prediction_result["boxes"]
-    elif crop_type == "poly":
-        regions = prediction_result["polys"]
-    else:
-        raise TypeError("crop_type can be only 'polys' or 'boxes'")
+class Craft:
+    def __init__(
+        self,
+        output_dir=None,
+        rectify=True,
+        export_extra=True,
+        text_threshold=0.7,
+        link_threshold=0.4,
+        low_text=0.4,
+        cuda=False,
+        long_size=1280,
+        refiner=True,
+        crop_type="poly",
+    ):
+        """
+        Arguments:
+            image_path: path to the image to be processed
+            output_dir: path to the results to be exported
+            rectify: rectify detected polygon by affine transform
+            export_extra: export heatmap, detection points, box visualization
+            text_threshold: text confidence threshold
+            link_threshold: link confidence threshold
+            low_text: text low-bound score
+            cuda: Use cuda for inference
+            long_size: desired longest image size for inference
+            refiner: enable link refiner
+            crop_type: crop regions by detected boxes or polys ("poly" or "box")
+        """
+        self.craft_net = None
+        self.refine_net = None
+        self.output_dir = output_dir
+        self.rectify = rectify
+        self.export_extra = export_extra
+        self.text_threshold = text_threshold
+        self.link_threshold = link_threshold
+        self.low_text = low_text
+        self.cuda = cuda
+        self.long_size = long_size
+        self.refiner = refiner
+        self.crop_type = crop_type
 
-    # export if output_dir is given
-    prediction_result["text_crop_paths"] = []
-    if output_dir is not None:
-        # export detected text regions
-        exported_file_paths = export_detected_regions(
-            image_path=image_path,
+        # load craftnet
+        self.load_craftnet_model()
+        # load refinernet if required
+        if refiner:
+            self.load_refinenet_model()
+
+    def load_craftnet_model(self):
+        """
+        Loads craftnet model
+        """
+        self.craft_net = load_craftnet_model(self.cuda)
+
+    def load_refinenet_model(self):
+        """
+        Loads refinenet model
+        """
+        self.refine_net = load_refinenet_model(self.cuda)
+
+    def unload_craftnet_model(self):
+        """
+        Unloads craftnet model
+        """
+        self.craft_net = None
+        craft_utils.empty_cuda_cache()
+
+    def unload_refinenet_model(self):
+        """
+        Unloads refinenet model
+        """
+        self.refine_net = None
+        craft_utils.empty_cuda_cache()
+
+    def detect_text(self, image_path):
+        """
+        Arguments:
+            image_path: path to the image to be processed
+        Output:
+            {"masks": lists of predicted masks 2d as bool array,
+            "boxes": list of coords of points of predicted boxes,
+            "boxes_as_ratios": list of coords of points of predicted boxes as ratios of image size,
+            "polys_as_ratios": list of coords of points of predicted polys as ratios of image size,
+            "heatmaps": visualization of the detected characters/links,
+            "text_crop_paths": list of paths of the exported text boxes/polys,
+            "times": elapsed times of the sub modules, in seconds}
+        """
+        # load image
+        image = read_image(image_path)
+
+        # perform prediction
+        prediction_result = get_prediction(
             image=image,
-            regions=regions,
-            output_dir=output_dir,
-            rectify=rectify,
+            craft_net=self.craft_net,
+            refine_net=self.refine_net,
+            text_threshold=self.text_threshold,
+            link_threshold=self.link_threshold,
+            low_text=self.low_text,
+            cuda=self.cuda,
+            long_size=self.long_size,
         )
-        prediction_result["text_crop_paths"] = exported_file_paths
 
-        # export heatmap, detection points, box visualization
-        if export_extra:
-            export_extra_results(
+        # arange regions
+        if self.crop_type == "box":
+            regions = prediction_result["boxes"]
+        elif self.crop_type == "poly":
+            regions = prediction_result["polys"]
+        else:
+            raise TypeError("crop_type can be only 'polys' or 'boxes'")
+
+        # export if output_dir is given
+        prediction_result["text_crop_paths"] = []
+        if self.output_dir is not None:
+            # export detected text regions
+            exported_file_paths = export_detected_regions(
                 image_path=image_path,
                 image=image,
                 regions=regions,
-                heatmaps=prediction_result["heatmaps"],
-                output_dir=output_dir,
+                output_dir=self.output_dir,
+                rectify=self.rectify,
             )
+            prediction_result["text_crop_paths"] = exported_file_paths
 
-    # return prediction results
-    return prediction_result
+            # export heatmap, detection points, box visualization
+            if self.export_extra:
+                export_extra_results(
+                    image_path=image_path,
+                    image=image,
+                    regions=regions,
+                    heatmaps=prediction_result["heatmaps"],
+                    output_dir=self.output_dir,
+                )
+
+        # return prediction results
+        return prediction_result
