6f99e1c2423bf5582a872ce71e4fe16c415d2e38
fcakyon <fcakyon@gmail.com>
2020-05-14 20:38:04 +0300

    sort imports

--- craft_text_detector/torch_utils.py
+++ craft_text_detector/torch_utils.py
@@ -1,7 +1,5 @@
+from torch import from_numpy, load, no_grad
 from torch.autograd import Variable
-from torch.nn import DataParallel
 from torch.backends.cudnn import benchmark as cudnn_benchmark
-from torch import load
-from torch import from_numpy
-from torch import no_grad
 from torch.cuda import empty_cache as empty_cuda_cache
+from torch.nn import DataParallel
